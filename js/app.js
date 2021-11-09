b2967b8730d9418e532a167676314f1d95a6726f
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-11-09 01:07:30 -0400

    Página de Cursos 3 Finalizada

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
+/* Funcionalidad  para el menu hamburguesa */
 let navbar = document.querySelector('.header .navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 }
 
+/* Funcionalidad que oculta el menu una ves se hace la acción de scroll */
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
+
+/* Funcionalidad para  el modal que reproduce los videos */
+let mainVid = document.querySelector('.main-video');
+
+document.querySelectorAll('.course-3 .box .video video').forEach(vid => {
+    vid.onclick = () => {
+        let src = vid.getAttribute('src');
+        mainVid.classList.add('active');
+        mainVid.querySelector('video').src = src;
+    }
+});
+
+/* Funcionalidad para cerrar el video */
+document.querySelector('#close-vid').onclick = () => {
+    mainVid.classList.remove('active');
+}
+
+
+
 
